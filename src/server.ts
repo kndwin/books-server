03100335a30d2b71de31a0a33ae70ffbabd80dca
Kevin Nguyen <me@kndwin.dev>
2021-07-18 20:42:44 +1000

    ➕ - added delete, edit and update mutations

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import { ApolloServer, gql } from 'apollo-server'
+import { Book, PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const typeDefs = gql`
 	type Book {
+		id: String
 		title: String
 		author: String
 	}
@@ -9,16 +13,32 @@ const typeDefs = gql`
 	type Query {
 		books: [Book]
 	}
-`
 
-const books = [
-	{ title: 'Great expectations', author: 'Charles Dickens' },
-	{ title: 'Below average expectations', author: 'Darles Chickens' }
-]
+	type Mutation {
+		addBook(title: String, author: String): Book
+		deleteBook(id: String): Book
+		editBook(id: String, title: String, author: String): Book
+
+	}
+`
 
 const resolvers = {
 	Query: {
-		books: () => books
+		books: () => { 
+			return prisma.book.findMany()
+		}
+	},
+
+	Mutation: {
+		addBook: (_: ParentNode, { title, author }: { title: string, author: string}) => {
+			return prisma.book.create({ data: { title, author} })
+		},
+		deleteBook: (_: ParentNode, { id }: { id: string}) => {
+			return prisma.book.delete({ where: { id }})
+		},
+		editBook: (_: ParentNode, { id, title, author}: {id: string, title?: string, author?: string}) => {
+			return prisma.book.update({ where: {id}, data: {title, author}})
+		}
 	}
 }
 
