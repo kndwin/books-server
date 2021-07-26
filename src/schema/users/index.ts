6c87a84427d2a63db0868e7d0bcd29421bdaf109
Kevin Nguyen <me@kndwin.dev>
2021-07-26 11:14:25 +1000

    🔨 - changed PEM to JWKS for auth

--- src/schema/users/index.ts
+++ src/schema/users/index.ts
@@ -2,7 +2,7 @@ import { gql } from "apollo-server";
 import { prisma } from "../../lib/prisma";
 import jwt from "jsonwebtoken";
 import fetch from "node-fetch";
-
+import { OAuth2Client } from "google-auth-library";
 export const typeDefs = gql`
   type User {
     id: String
@@ -29,18 +29,45 @@ export const resolvers = {
     },
     authorize: async (_: ParentNode, { token }: { token: string }) => {
       try {
-        // get google's public key
-        const res = await fetch("https://www.googleapis.com/oauth2/v1/certs");
-        const publicKey = await res.json();
-        // verify JWT token
-        const verified = jwt.verify(
-          token,
-          publicKey["7f548f6708690c21120b0ab668caa079acbc2b2f"],
-          { algorithms: ["RS256"] }
+        const client = new OAuth2Client(
+          "682101596809-2a0n5asmn8vmk3md35r3v8vbfeb4th0k.apps.googleusercontent.com"
         );
+        async function verify() {
+          const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience:
+              "682101596809-2a0n5asmn8vmk3md35r3v8vbfeb4th0k.apps.googleusercontent.com", // Specify the CLIENT_ID of the app that accesses the backend
+            // Or, if multiple clients access the backend:
+            //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+          });
+          const payload = ticket.getPayload();
+          const userid = payload?.sub;
+          console.log({ userid });
+          // If request specified a G Suite domain:
+          // const domain = payload['hd'];
+        }
+        verify().catch(console.error);
+
+        /*
+        // get google's public key
+        //        const publicKey = await res.json();
+        //        console.log(publicKey.keys[0]);
+        var jwksClient = require("jwks-rsa");
+        var client = jwksClient({
+          jwksUri: "https://www.googleapis.com/oauth2/v3/certs",
+        });
+        const res = await fetch("https://www.googleapis.com/oauth2/v3/certs");
+        const header = res.headers;
+        console.log({ header });
+        const json = await res.json();
+        const kid = json.keys[1].kid;
+        const key = await client.getSigningKey(kid);
+        const signingKey = key.getPublicKey();
+
+        const verified = jwt.verify(token, signingKey); // verify JWT token
         // @ts-ignore
-        const { email } = verified;
-        return prisma.user.findFirst({ where: { email } });
+				*/
+        return prisma.user.findFirst({ where: { email: "test" } });
       } catch (err) {
         return err;
       }
